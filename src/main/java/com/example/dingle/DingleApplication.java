fc6713c6ad35c164f57e335a047d9fd131519942
kung036 <sinariari3@gmail.com>
2023-09-04 19:24:00 +0900

    Initial commit

---
+++ src/main/java/com/example/dingle/DingleApplication.java
@@ -0,0 +1,13 @@
+package com.example.dingle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DingleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DingleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dingle/DingleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dingle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DingleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
