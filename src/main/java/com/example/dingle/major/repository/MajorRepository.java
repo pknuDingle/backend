71d80228e45043ed325137ac9ae4495c9acb5be1
JeongeunChoi <77786996+JeongeunChoi@users.noreply.github.com>
2023-09-18 18:14:55 +0900

    Revert "feat - #1 : CRUD"

--- src/main/java/com/example/dingle/major/repository/MajorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.dingle.major.repository;
-
-import com.example.dingle.major.entity.Major;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MajorRepository extends JpaRepository<Major, Long> {
-}
--- src/main/java/com/example/dingle/userCategory/repository/UserCategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.dingle.userCategory.repository;
-
-import com.example.dingle.userCategory.entity.UserCategory;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserCategoryRepository extends JpaRepository<UserCategory, Long> {
-}
