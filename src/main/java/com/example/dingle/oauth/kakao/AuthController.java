bfa9e4f81879a7bd7871fc6ef398603aa85acc2f
kung036 <sinariari3@gmail.com>
2023-11-16 20:40:47 +0900

    feat - #14 : realTime crawling
    - 실시간 크롤링
    - 학교 메인 홈페이지만 테스트

--- src/main/java/com/example/dingle/oauth/kakao/AuthController.java
+++ src/main/java/com/example/dingle/oauth/kakao/AuthController.java
@@ -3,14 +3,13 @@ package com.example.dingle.oauth.kakao;
 import com.example.dingle.oauth.jwt.JwtTokenProvider;
 import com.example.dingle.user.entity.User;
 import com.example.dingle.user.mapper.UserMapper;
+import com.example.dingle.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.constraints.Positive;
 import java.util.HashMap;
 
 @RequiredArgsConstructor
@@ -20,6 +19,7 @@ public class AuthController {
     private final KakaoAuthService kakaoAuthService;
     private final JwtTokenProvider jwtTokenProvider;
     private final UserMapper userMapper;
+    private final UserService userService;
 
     // 카카오 로그인을 위해 회원가입 여부 확인
     //  -> 이미 회원이면 JWT 토큰 발급
@@ -28,6 +28,15 @@ public class AuthController {
     public ResponseEntity authSignin(@RequestHeader String accessToken) {
         User user = kakaoAuthService.signin(accessToken);
         String jwt = jwtTokenProvider.createToken(String.valueOf(user.getId()));
+        KakaoResponseDto.Response response = userMapper.userToKakaoResponseDtoResponse(user, jwt);
+        return new ResponseEntity<>(response, HttpStatus.ACCEPTED);
+    }
+
+    // token 사용을 위한 테스크 과정
+    @PostMapping("/test/{userId}")
+    public ResponseEntity generate(@Positive @PathVariable long userId) {
+        User user = userService.verifiedUser(userId);
+        String jwt = jwtTokenProvider.createToken(String.valueOf(userId));
         KakaoResponseDto.Response response = userMapper.userToKakaoResponseDtoResponse(user, jwt);
         return new ResponseEntity<>(response, HttpStatus.ACCEPTED);
     }
