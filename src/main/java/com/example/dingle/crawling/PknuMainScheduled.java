721cd215293f9fb0c266d25c50ed382c8cd3c600
kung036 <sinariari3@gmail.com>
2023-11-28 14:12:10 +0900

    fix - #32 : 메인홈페이지 크롤링 에러
    - 메인 홈페이지가 크롤링되지 않은 에러 수정
    - 방법 -> 컴퓨터인공지능공학부와 동일하게 변경

--- src/main/java/com/example/dingle/crawling/PknuMainScheduled.java
+++ src/main/java/com/example/dingle/crawling/PknuMainScheduled.java
@@ -13,61 +13,15 @@ import java.util.List;
 @Component
 //@AllArgsConstructor
 public class PknuMainScheduled {
-    private final NoticeRepository noticeRepository;
     private final PknuMain pknuMain;
 
-    private final Filtering filtering;
-
-    private final NoticeKeywordService noticeKeywordService;
-
-    private static long pageNum = 0;
-
-    public PknuMainScheduled(NoticeRepository noticeRepository, PknuMain pknuMain, Filtering filtering, NoticeKeywordService noticeKeywordService) {
-        this.noticeRepository = noticeRepository;
+    public PknuMainScheduled(PknuMain pknuMain) {
         this.pknuMain = pknuMain;
-        this.filtering = filtering;
-        this.noticeKeywordService = noticeKeywordService;
     }
 
     // 실시간 실행 테스트(1시간 단위)
     @Scheduled(fixedDelay = 3600000) // 1초(=1000)
     public void pknuMainScheduled() {
-        // 가장 최근에 가져온 페이지 번호 설정
-        if(pageNum == 0) {
-            List<Notice> notices = noticeRepository.findByHomepage(pknuMain.findPknuMainHomepage());
-            if(notices.isEmpty()) pageNum = 713048; // DB가 비어있는 경우
-            else pageNum = notices.get(notices.size() - 1).getPageNum();
-        }
-
-        // 삭제된 공지사항을 대비해서 최근 저장한 공지사항에서 3번째까지 비교
-        int count = 1;
-        while(count < 4) {
-            count = noticeCheck(count);
-        }
-    }
-
-    // 빈 페이지 경우를 대비해서 만듦
-    public int noticeCheck(int count) {
-        Notice notice = pknuMain.crawling(pageNum + count);
-
-        // 새로운 공지사항이 있는 경우
-        if(notice != null) {
-            noticeRepository.save(notice);
-
-            // 키워드 설정
-            List<Keyword> keywords = filtering.filterKeywordsReturnKeyWord(notice.getTitle(), notice.getContent());
-            noticeKeywordService.createNoticeKeyword(notice, keywords);
-
-            // TODO: 알림 전송
-
-
-
-            pageNum += count;
-            return 10;
-        } else {
-            System.out.println("!! pageNum : " + pageNum);
-        }
-
-        return count+1;
+        pknuMain.crawling();
     }
 }
