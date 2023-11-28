3954982fe9d7182bd2eaa1f79cc436f411008e81
kung036 <sinariari3@gmail.com>
2023-11-28 14:11:24 +0900

    fix - #32 : 컴퓨터인공지능공학부 크롤링 수정
    - 홈페이지 연관관계 설정
    - image 추출

--- src/main/java/com/example/dingle/crawling/PknuCe.java
+++ src/main/java/com/example/dingle/crawling/PknuCe.java
@@ -1,5 +1,7 @@
 package com.example.dingle.crawling;
 
+import com.example.dingle.homepage.entity.Homepage;
+import com.example.dingle.homepage.service.HomepageService;
 import com.example.dingle.keyword.entity.Keyword;
 import com.example.dingle.keyword.repository.KeywordRepository;
 import com.example.dingle.notice.entity.Notice;
@@ -25,6 +27,9 @@ import java.util.List;
 @Component
 public class PknuCe {
     private final static String USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.152 Safari/537.36";
+    private final static String HOMEPAGE_NAME = "부경대학교 컴퓨터·인공지능공학부";
+    private final static String HOMEPAGE_URL = "https://ce.pknu.ac.kr/ce/1814";
+    private final static String IMAGE_URL = "https://ce.pknu.ac.kr/";
     private String pknuCeAllNoticeUrl = "https://ce.pknu.ac.kr/ce/1814";
     private final NoticeRepository noticeRepository;
     private final NoticeKeywordRepository noticeKeywordRepository;
@@ -33,20 +38,21 @@ public class PknuCe {
     private final UserKeywordService userKeywordService;
     private final PersonalNoticeService personalNoticeService;
     private final NoticeKeywordService noticeKeywordService;
+    private final HomepageService homepageService;
 
-    public PknuCe(NoticeRepository noticeRepository,
-                  NoticeKeywordRepository noticeKeywordRepository,
-                  KeywordRepository keywordRepository, Filtering filtering, PersonalNoticeService personalNoticeService, UserKeywordRepository userKeywordRepository, UserKeywordService userKeywordService, PersonalNoticeService personalNoticeService1, NoticeKeywordService noticeKeywordService) {
+    public PknuCe(NoticeRepository noticeRepository, NoticeKeywordRepository noticeKeywordRepository, KeywordRepository keywordRepository, Filtering filtering, UserKeywordService userKeywordService, PersonalNoticeService personalNoticeService, NoticeKeywordService noticeKeywordService, HomepageService homepageService) {
         this.noticeRepository = noticeRepository;
         this.noticeKeywordRepository = noticeKeywordRepository;
         this.keywordRepository = keywordRepository;
         this.filtering = filtering;
         this.userKeywordService = userKeywordService;
-        this.personalNoticeService = personalNoticeService1;
+        this.personalNoticeService = personalNoticeService;
         this.noticeKeywordService = noticeKeywordService;
+        this.homepageService = homepageService;
     }
 
     public void pknuCeAllNoticeCrawling() throws Exception {
+        Homepage homepage = findPknuMainHomepage();
         if (pknuCeAllNoticeUrl.indexOf("https://") >= 0) {
             PknuCe.setSSL();
         }
@@ -62,7 +68,9 @@ public class PknuCe {
         for (Element notice : notices) {
             // 번호, 제목, 링크 가져오기
             Long noticeNum = Long.valueOf(notice.select("td.bdlNum.noti").text());
-            if (noticeNum <= latestNoticeNum) continue; // db에 저장된 가장 최신 공지사항 번호보다 크롤링한 공지사항 번호가 작은 경우 패스
+//            if (noticeNum <= latestNoticeNum) continue; // db에 저장된 가장 최신 공지사항 번호보다 크롤링한 공지사항 번호가 작은 경우 패스
+            System.out.println("!! " + noticeNum);
+            if(noticeNum != 1190) continue;
             String title = notice.select("td.bdlTitle a").text();
             String link = pknuCeAllNoticeUrl + notice.select("td.bdlTitle a").attr("href");
 
@@ -72,16 +80,44 @@ public class PknuCe {
             String text = doc.select("td.bdvEdit").text();
             String content = text.length() > 30 ? text.substring(0, 30) + "..." : text;
 
+            // 이미지
+            Element element = doc.select("td.bdvEdit").first();
+            String image = IMAGE_URL + element.select("img").attr("src"); // 이미지
+
             // 저장
-            Notice newNotice = noticeRepository.save(new Notice(title, content, link, noticeNum));
+            Notice newNotice = new Notice();
+            newNotice.setHomepage(homepage);
+            newNotice.setNoticeNum(noticeNum);
+            newNotice.setLink(link);
+            newNotice.setTitle(title);
+            newNotice.setContent(content);
+            newNotice.setImage(image);
+            newNotice = noticeRepository.save(newNotice);
 
             // 키워드 기반 필터링(제목, 내용에서 키워드 추출 -> 키워드 설정한 user에게 알림 전송)
-            List<Keyword> keywords = filtering.filterKeywordsReturnKeyWord(title, content);
+            List<Keyword> keywords = filtering.filterKeywordsReturnKeyWord(title, text);
             noticeKeywordService.createNoticeKeyword(newNotice, keywords);
             List<UserKeyword> userKeywords = userKeywordService.findUserKeywordsWithKeywords(keywords);
             personalNoticeService.createPersonalNotices(userKeywords, newNotice);
             // TODO: 알림 전송
         }
+    }
+
+    // 연관관계 있는 홈페이지 반환
+    public Homepage findPknuMainHomepage() {
+        List<Homepage> homepages = homepageService.findAllHomepages();
+        Homepage mainHomepage = null;
+        for (Homepage homepage : homepages) {
+            if (homepage.getName().equals(HOMEPAGE_NAME)) mainHomepage = homepage;
+        }
+        if (mainHomepage == null) { // 부경대 메인 홈페이지가 없는 경우 -> 새로 생성하기
+            mainHomepage = new Homepage();
+            mainHomepage.setName(HOMEPAGE_NAME);
+            mainHomepage.setUrl(HOMEPAGE_URL);
+            mainHomepage = homepageService.createHomepage(mainHomepage);
+        }
+
+        return mainHomepage;
     }
 
     // SSL 우회 등록
