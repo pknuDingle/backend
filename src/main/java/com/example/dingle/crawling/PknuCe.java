e237f264359d62c45ce8c2b874b777157d330d30
JeongeunChoi <77786996+JeongeunChoi@users.noreply.github.com>
2023-11-29 01:51:55 +0900

    feat: 부경대 컴공 홈페이지에 FCM 기능 추가

--- src/main/java/com/example/dingle/crawling/PknuCe.java
+++ src/main/java/com/example/dingle/crawling/PknuCe.java
@@ -1,15 +1,19 @@
 package com.example.dingle.crawling;
 
+import com.example.dingle.fcm.service.FcmService;
+import com.example.dingle.homepage.entity.Homepage;
+import com.example.dingle.homepage.repository.HomepageRepository;
 import com.example.dingle.keyword.entity.Keyword;
-import com.example.dingle.keyword.repository.KeywordRepository;
 import com.example.dingle.notice.entity.Notice;
 import com.example.dingle.notice.repository.NoticeRepository;
-import com.example.dingle.noticeKeyword.repository.NoticeKeywordRepository;
 import com.example.dingle.noticeKeyword.service.NoticeKeywordService;
+import com.example.dingle.personalNotice.entity.PersonalNotice;
 import com.example.dingle.personalNotice.service.PersonalNoticeService;
+import com.example.dingle.userHomepage.entity.UserHomepage;
+import com.example.dingle.userHomepage.repository.UserHomepageRepository;
 import com.example.dingle.userKeyword.entity.UserKeyword;
 import com.example.dingle.userKeyword.repository.UserKeywordRepository;
-import com.example.dingle.userKeyword.service.UserKeywordService;
+import lombok.RequiredArgsConstructor;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -17,72 +21,28 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.springframework.stereotype.Component;
 
-import javax.net.ssl.*;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 import java.security.SecureRandom;
 import java.security.cert.X509Certificate;
 import java.util.List;
 
 @Component
+@RequiredArgsConstructor
 public class PknuCe {
     private final static String USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.152 Safari/537.36";
-    private String pknuCeAllNoticeUrl = "https://ce.pknu.ac.kr/ce/1814";
     private final NoticeRepository noticeRepository;
-    private final NoticeKeywordRepository noticeKeywordRepository;
-    private final KeywordRepository keywordRepository;
     private final Filtering filtering;
-    private final UserKeywordService userKeywordService;
+    private final UserKeywordRepository userKeywordRepository;
     private final PersonalNoticeService personalNoticeService;
     private final NoticeKeywordService noticeKeywordService;
-
-    public PknuCe(NoticeRepository noticeRepository,
-                  NoticeKeywordRepository noticeKeywordRepository,
-                  KeywordRepository keywordRepository, Filtering filtering, PersonalNoticeService personalNoticeService, UserKeywordRepository userKeywordRepository, UserKeywordService userKeywordService, PersonalNoticeService personalNoticeService1, NoticeKeywordService noticeKeywordService) {
-        this.noticeRepository = noticeRepository;
-        this.noticeKeywordRepository = noticeKeywordRepository;
-        this.keywordRepository = keywordRepository;
-        this.filtering = filtering;
-        this.userKeywordService = userKeywordService;
-        this.personalNoticeService = personalNoticeService1;
-        this.noticeKeywordService = noticeKeywordService;
-    }
-
-    public void pknuCeAllNoticeCrawling() throws Exception {
-        if (pknuCeAllNoticeUrl.indexOf("https://") >= 0) {
-            PknuCe.setSSL();
-        }
-
-        Connection listConnection, elementConnection;
-        listConnection = Jsoup.connect(pknuCeAllNoticeUrl).header("Content-Type", "application/json;charset=UTF-8").userAgent(USER_AGENT).method(Connection.Method.GET).ignoreContentType(true);
-
-        // db에 저장된 가장 최신 공지사항 번호
-        Long latestNoticeNum = noticeRepository.findLatestNoticeNum();
-
-        // 공지사항 리스트
-        Elements notices = listConnection.get().select(".a_bdCont .a_brdList tbody tr:not(.noti)");
-        for (Element notice : notices) {
-            // 번호, 제목, 링크 가져오기
-            Long noticeNum = Long.valueOf(notice.select("td.bdlNum.noti").text());
-            if (noticeNum <= latestNoticeNum) continue; // db에 저장된 가장 최신 공지사항 번호보다 크롤링한 공지사항 번호가 작은 경우 패스
-            String title = notice.select("td.bdlTitle a").text();
-            String link = pknuCeAllNoticeUrl + notice.select("td.bdlTitle a").attr("href");
-
-            // 링크를 통해 내용 일부 가져오기
-            elementConnection = Jsoup.connect(link).header("Content-Type", "application/json;charset=UTF-8").userAgent(USER_AGENT).method(Connection.Method.GET).ignoreContentType(true);
-            Document doc = Jsoup.parse(elementConnection.get().html());
-            String text = doc.select("td.bdvEdit").text();
-            String content = text.length() > 30 ? text.substring(0, 30) + "..." : text;
-
-            // 저장
-            Notice newNotice = noticeRepository.save(new Notice(title, content, link, noticeNum));
-
-            // 키워드 기반 필터링(제목, 내용에서 키워드 추출 -> 키워드 설정한 user에게 알림 전송)
-            List<Keyword> keywords = filtering.filterKeywordsReturnKeyWord(title, content);
-            noticeKeywordService.createNoticeKeyword(newNotice, keywords);
-            List<UserKeyword> userKeywords = userKeywordService.findUserKeywordsWithKeywords(keywords);
-            personalNoticeService.createPersonalNotices(userKeywords, newNotice);
-            // TODO: 알림 전송
-        }
-    }
+    private final FcmService fcmService;
+    private final HomepageRepository homepageRepository;
+    private final UserHomepageRepository userHomepageRepository;
+    private final long pknuCeHomepageId = 2;
+    private final String pknuCeAllNoticeUrl = "https://ce.pknu.ac.kr/ce/1814";
 
     // SSL 우회 등록
     public static void setSSL() throws Exception {
@@ -105,5 +65,51 @@ public class PknuCe {
 
         HttpsURLConnection.setDefaultHostnameVerifier((hostname, session) -> true);
         HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+    }
+
+    public void pknuCeAllNoticeCrawling() throws Exception {
+        if (pknuCeAllNoticeUrl.indexOf("https://") >= 0) {
+            PknuCe.setSSL();
+        }
+
+        Connection listConnection, elementConnection;
+        listConnection = Jsoup.connect(pknuCeAllNoticeUrl).header("Content-Type", "application/json;charset=UTF-8").userAgent(USER_AGENT).method(Connection.Method.GET).ignoreContentType(true);
+
+        Homepage pknuCeHomepage = homepageRepository.findById(pknuCeHomepageId).get();
+        // db에 저장된 가장 최신 공지사항 번호
+        Long latestNoticeNum = noticeRepository.findLatestNoticeNum();
+
+        // 공지사항 리스트
+        Elements notices = listConnection.get().select(".a_bdCont .a_brdList tbody tr:not(.noti)");
+        for (Element notice : notices) {
+            // 번호, 제목, 링크 가져오기
+            Long noticeNum = Long.valueOf(notice.select("td.bdlNum.noti").text());
+            if (noticeNum <= latestNoticeNum) continue; // db에 저장된 가장 최신 공지사항 번호보다 크롤링한 공지사항 번호가 작은 경우 패스
+            String title = notice.select("td.bdlTitle a").text();
+            String link = pknuCeAllNoticeUrl + notice.select("td.bdlTitle a").attr("href");
+
+            // 링크를 통해 내용 일부 가져오기
+            elementConnection = Jsoup.connect(link).header("Content-Type", "application/json;charset=UTF-8").userAgent(USER_AGENT).method(Connection.Method.GET).ignoreContentType(true);
+            Document doc = Jsoup.parse(elementConnection.get().html());
+            String text = doc.select("td.bdvEdit").text();
+            String content = text.length() > 30 ? text.substring(0, 30) + "..." : text;
+
+            // 저장
+            Notice newNotice = noticeRepository.save(new Notice(title, content, link, noticeNum, pknuCeHomepage));
+
+            // 키워드 기반 필터링
+            List<Keyword> keywords = filtering.filterKeywordsReturnKeyWord(title, content);
+            noticeKeywordService.createNoticeKeyword(newNotice, keywords);
+
+            // 설정된 키워드 공지사항 알림
+            List<UserKeyword> userKeywords = userKeywordRepository.findAllByKeywordIn(keywords);
+            List<PersonalNotice> personalKeywordNotices = personalNoticeService.createPersonalNoticesWithUserKeywords(userKeywords, newNotice);
+            fcmService.sendPersonalKeywordNoticeToUser(personalKeywordNotices);
+
+            // 설정된 홈페이지 공지사항 알림
+            List<UserHomepage> userHomepages = userHomepageRepository.findAllByHomepage(pknuCeHomepage);
+            List<PersonalNotice> personalHomepageNotices = personalNoticeService.createPersonalNoticesWithUserHomepages(userHomepages, newNotice);
+            fcmService.sendPersonalHomepageNoticeToUser(personalHomepageNotices);
+        }
     }
 }
