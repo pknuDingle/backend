f1666e9a0973ae934e2f402b4cf616ce0f28156e
JeongeunChoi <77786996+JeongeunChoi@users.noreply.github.com>
2023-11-29 01:19:27 +0900

    feat - #31 : FcmMessageConverter 구현
    - PersonalNotice를 MulticastMessage로 변환

---
+++ src/main/java/com/example/dingle/fcm/converter/FcmMessageConverter.java
@@ -0,0 +1,40 @@
+package com.example.dingle.fcm.converter;
+
+import com.example.dingle.personalNotice.entity.PersonalNotice;
+import com.google.firebase.messaging.MulticastMessage;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class FcmMessageConverter {
+
+    public MulticastMessage personalKeywordNoticeToMessage(PersonalNotice personalNotice, List<String> fcmTokens) {
+        return MulticastMessage.builder()
+                .putData("keyword", personalNotice.getKeyword().getName())
+                .putData("title", personalNotice.getNotice().getTitle())
+                .putData("content", personalNotice.getNotice().getContent().substring(0, 30) + "...")
+                .putData("link", personalNotice.getNotice().getLink())
+                .addAllTokens(fcmTokens)
+                .build();
+    }
+
+    public MulticastMessage personalHomepageNoticeToMessage(PersonalNotice personalNotice, List<String> fcmTokens) {
+        return MulticastMessage.builder()
+                .putData("homepage", personalNotice.getHomepage().getName())
+                .putData("title", personalNotice.getNotice().getTitle())
+                .putData("content", personalNotice.getNotice().getContent().substring(0, 30) + "...")
+                .putData("link", personalNotice.getNotice().getLink())
+                .addAllTokens(fcmTokens)
+                .build();
+    }
+
+    public MulticastMessage toNotice(String title, String content, List<String> allUserTokens) {
+        return MulticastMessage.builder()
+                .putData("type", "notice")
+                .putData("title", title)
+                .putData("body", content)
+                .addAllTokens(allUserTokens)
+                .build();
+    }
+}
