fd041fa0dda7288025e697c7ef05fddafa59ced9
JeongeunChoi <77786996+JeongeunChoi@users.noreply.github.com>
2023-12-01 00:27:28 +0900

    feat - #31 : 생성자 어노테이션 수정 및 추가

--- src/main/java/com/example/dingle/crawling/PknuCeScheduled.java
+++ src/main/java/com/example/dingle/crawling/PknuCeScheduled.java
@@ -1,20 +1,14 @@
 package com.example.dingle.crawling;
 
-import com.example.dingle.noticeKeyword.service.NoticeKeywordService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class PknuCeScheduled {
-    private final PknuCe pknuCe;
-    private final Filtering filtering;
-    private final NoticeKeywordService noticeKeywordService;
 
-    public PknuCeScheduled(PknuCe pknuCe, Filtering filtering, NoticeKeywordService noticeKeywordService) {
-        this.pknuCe = pknuCe;
-        this.filtering = filtering;
-        this.noticeKeywordService = noticeKeywordService;
-    }
+    private final PknuCe pknuCe;
 
     // 실시간 실행 테스트(1시간 단위)
     @Scheduled(fixedDelay = 3600000)
